cf30ee824f53acf9909e94511df5d0954a28f3d7
Clarence <yecong2302230@163.com>
2017-08-15 15:22:12 +0800

    change

--- test3.py
+++ test3.py
@@ -1,9 +1,11 @@
-from matplotlib import pyplot as plt
-
-from matplotlib.path import Path
-import matplotlib.patches as patches
-
-plt.plot([0,2,2,0],[0,0,2,0])
-patches()
-
-plt.show()
+#!/usr/bin/env python3
+amount = float(input("Enter amount: "))  # 输入数额
+inrate = float(input("Enter Interest rate: "))  # 输入利率
+period = int(input("Enter period: "))  # 输入期限
+value = 0
+year = 1
+while year <= period:
+    value = amount + (inrate * amount)
+    print("Year {} Rs. {:.2f}".format(year, value))
+    amount = value
+    year = year + 1
